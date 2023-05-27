f080dad1fe88151c950c74850b711e4db35935e3
kuntsevn <nevporu@mail.ru>
2023-05-27 16:39:30 +0300

    переменная старт айфона переименована

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ alert(a);
 a = 20; // добавлено 27.05.23
 alert (a); // добавлено 27.05.23
 
-const b = 2007; //2
-alert(`год выпуска 1го айфона ${b}`);
+const IphoneStartYear = 2007; //2
+alert(`год выпуска 1го айфона ${IphoneStartYear}`);
 
 const c = 'Брендан Эйх'; //3
 alert(`значение имени создателя языка JavaScript ${c}`)

b75034e72890285251f81b90e4def1302c647e5e
kuntsevn <nevporu@mail.ru>
2023-07-16 22:23:56 +0300

    2.5  HW start

---
+++ HW5.js
@@ -0,0 +1,52 @@
+//Задание 2.5.1
+/*
+function min(a, b) { return a <= b? a : b; }
+var x = 8, y = 4, m;
+m = min(x, y);
+console.log(m);
+
+function min(a, b) { return a <= b? a : b; }
+var x = 6, y = 6, m;
+m = min(x, y);
+console.log(m);
+
+//Задание 2.5.2
+
+
+
+function even_or_odd(number) {
+    return number % 2 === 0 ? "Odd" : "Even"
+}
+
+console.log(even_or_odd(3));
+console.log(even_or_odd(2));
+
+
+
+//Задание 2.5.3
+
+// 3.1
+
+function square(num) {
+return num * num;
+}
+ let num = 5;
+ console.log(square(num));
+
+// 3.2
+
+
+ function square(numb) {
+    return numb * numb;
+    }
+
+    let numb = 4;
+
+    alert (square(numb))
+*/
+
+   //Задание 2.5.4 
+
+
+
+   

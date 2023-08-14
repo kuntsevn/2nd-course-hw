0ae2469d7c6a1a62ee5cea06ab2eb326517cd8db
kuntsevn <nevporu@mail.ru>
2023-08-15 00:41:34 +0300

    HW2.6 several tasks completed

--- HW6.js
+++ HW6.js
@@ -43,17 +43,47 @@ let numbs2 = [2, 2, 2];
 let numbs3 =numbs.concat(numbs2);
 console.log(numbs3);
 
-*/
+
 //задание 6.6
 
 let arr = [9, 8, 7, 'a', 6, 5];
-
+arr.sort();
+arr.pop();
+console.log(arr);
 
 
 //задание 6.7
+
+
+const arr = [9, 8, 7, 6, 5];
+let number = prompt('Введите число от 1 до 10');
+
+if (arr.includes(parseInt(number))) {
+alert("угадал");
+} else {
+alert("не угадал");
+}
 //задание 6.8
+
+let smth = 'abcdef';
+let arrSmth = smth.split('');
+arrSmth.reverse ();
+arrSmth = arrSmth.join('');
+console.log(arrSmth);
+
+
 //задание 6.9
+
+const arr = [[1, 2, 3,],[4, 5, 6]];
+const arr1 = arr.flat();
+console.log(arr1);
+
+*/
+
 //задание 6.10
+
+
+
 //задание 6.11
 //задание 6.12
 //задание 6.13

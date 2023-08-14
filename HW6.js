c09be4c32e2de321af23b0f04c63390ea7f3f924
kuntsevn <nevporu@mail.ru>
2023-08-15 01:31:41 +0300

    HW 2.6 some tasks have been completed

--- HW6.js
+++ HW6.js
@@ -1,7 +1,6 @@
-
+/*
 //задание 6.1
 
-/*
 const numbs = [1, 5, 4, 10, 0, 3];
 
 for (let i = 0; i < numbs.length; i++) {
@@ -14,13 +13,11 @@ for (let i = 0; i < numbs.length; i++) {
 let numbers = [1, 5, 4, 10, 0, 3];
 console.log(numbers.indexOf(4));  //2
 
-
 //задание 6.3
 
 let numbs = [1, 3, 5, 10, 20];
 numbs = numbs.join('  ');
 console.log(numbs); // 1 3 5 10 20
-
 
 //задание 6.4
 
@@ -35,14 +32,12 @@ for (const inArr of arr) {
 }
 console.log(outPutArr);
 
-
 //задание 6.5
 
 let numbs = [1, 1, 1];
 let numbs2 = [2, 2, 2];
 let numbs3 =numbs.concat(numbs2);
 console.log(numbs3);
-
 
 //задание 6.6
 
@@ -51,9 +46,7 @@ arr.sort();
 arr.pop();
 console.log(arr);
 
-
 //задание 6.7
-
 
 const arr = [9, 8, 7, 6, 5];
 let number = prompt('Введите число от 1 до 10');
@@ -63,6 +56,7 @@ alert("угадал");
 } else {
 alert("не угадал");
 }
+
 //задание 6.8
 
 let smth = 'abcdef';
@@ -71,19 +65,45 @@ arrSmth.reverse ();
 arrSmth = arrSmth.join('');
 console.log(arrSmth);
 
-
 //задание 6.9
 
 const arr = [[1, 2, 3,],[4, 5, 6]];
 const arr1 = arr.flat();
 console.log(arr1);
 
-*/
-
 //задание 6.10
 
-
+let random = [4, 7, 1, 3];
+for (let i = 0; i < (random.length -1); i++) {
+    console.log(random[i] + random[i+1])
+};
+console.log(random[(random.length -1)]);
 
 //задание 6.11
+
+let random = [4, 7, 1, 3];
+let result = random.map(item => (item **2));
+console.log(result);
+
 //задание 6.12
+
+const str = ['бам','бум','бааах','бух'];
+const getLength = source => source.map (str => str.length);
+console.log(getLength(['бам','бум','бааах','бух']));
+
+
 //задание 6.13
+
+function filterPositive(array) {
+
+const filterPositive = (array) => array.filter(value => value < 0);
+
+var positives = filterPositive (array);
+
+console.log(positives);}
+
+filterPositive([-1, 0, 5, -10, 56]); // => [-1, -10]
+filterPositive([-25, 25, 0, -1000, -2]); // => [-25, -1000, -2]
+
+
+*/

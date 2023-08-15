0b04b47de64194f8bc696a113a9787fd9c788042
kuntsevn <nevporu@mail.ru>
2023-08-15 20:18:42 +0300

    task 2.6 2nd try

---
+++ HW7.js
@@ -0,0 +1,66 @@
+// задание 7.1
+/*
+let str = 'строка';
+str = str.toUpperCase();
+console.log(str);
+
+// задание 7.2
+
+function arrAndStr(arr, str) {
+    let newArr = [];
+    arr.forEach((el) => {
+        if (el.toLowerCase().startsWith(str.toLowerCase())) {
+            newArr.push(el);
+        }
+    })
+    console.log(newArr);
+}
+
+arrAndStr(['Дом', 'Банк', 'Больница', 'Театр'], 'Б');
+
+// задание 7.3
+
+let number = 32.58884
+Math.floor (number);
+Math.ceil (number);
+Math.round (number);
+console.log(Math.floor(number), Math.ceil(number), Math.round(number));
+
+
+// задание 7.4
+
+console.log(Math.min(52, 53, 49, 77, 21, 32), Math.max(52, 53, 49, 77, 21, 32));
+
+
+// задание 7.5
+
+function getRandomInt(minValue, maxValue) {
+    return Math.round(Math.random()*(maxValue-minValue))+minValue
+}
+console.log(getRandomInt (1, 10));
+
+// задание 7.6
+
+function getRandomArrNumbers(num) {
+    return (Array.from({length: num / 2}, () => Math.round(Math.random() * num)));  
+}
+console.log(getRandomArrNumbers(4));
+*/
+// задание 7.7
+
+function getRandomInt(minValue, maxValue) {
+    return Math.round(Math.random()*(maxValue-minValue))+minValue
+}
+console.log(getRandomInt (6, 10));
+
+
+// задание 7.8
+
+
+// задание 7.9
+
+
+// задание 7.10
+
+
+// задание 7.11

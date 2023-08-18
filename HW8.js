8ba5dac9f456f8abf8fe5f31c593b8ccd70e7617
kuntsevn <nevporu@mail.ru>
2023-08-18 07:17:00 +0300

    2.8 in process

--- HW8.js
+++ HW8.js
@@ -1,14 +1,5 @@
-// задание 1
 /*
-const people = [
-    { name: 'Глеб', age: 29 },
-    { name: 'Анна', age: 17 },
-    { name: 'Олег', age: 7 },
-    { name: 'Оксана', age: 47 }
- ];
-*/
 // задание 2
-
 
 function isPositive(num) {
     return num > 0;
@@ -38,9 +29,54 @@ function isPositive(num) {
   ];
   
   console.log(filter(people, isMale)); // Должен выводить [{name: 'Глеб', gender: 'male'}, {name: 'Олег', gender: 'male'}]
-  
-// задание 3
+
+  // задание 3
+
+
+var timerId = setInterval(() => console.log(new Date), 3000);
+
+setTimeout(() => { clearInterval(timerId); alert('30 секунд прошло'); }, 30000);
 
 // задание 4
 
+function delayForSecond(callback) {
+    setTimeout(callback, 1000)
+}
+delayForSecond (function() {
+    console.log('Privet, Gleb');
+})
+
 // задание 5
+
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+				if(cb) { 	cb(); }
+
+    }, 1000)
+}
+function sayHi (name) {
+    console.log(`Привет, ${name}!`);
+}
+setTimeout(() => {
+    return sayHi('Gleb');
+}, 2000); delayForSecond();
+
+// задание 1
+
+
+const people = [
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+ ];
+
+
+
+
+ 
+
+*/
+
+
